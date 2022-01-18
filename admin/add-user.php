beb463db28d524cea34b24e3f14775606bcc568a
Manish kumar <manishkumarchaurasiyagpj@gmail.com>
2022-01-18 23:03:48 +0530

    add user completed

--- admin/add-user.php
+++ admin/add-user.php
@@ -1,4 +1,29 @@
-<?php include "header.php"; ?>
+<?php
+include "header.php";
+include "conn.php";
+if (!isset($_SESSION['username'])) {
+    header('location:index.php');
+}
+$roles = mysqli_query($conn,"SELECT * FROM roles");
+if(isset($_POST['save'])){
+    $fname=$_POST['fname'];
+    $lname=$_POST['lname'];
+    $email=$_POST['email'];
+    $password=$_POST['password'];
+    $role=$_POST['role'];
+
+    $check = mysqli_query($conn,"SELECT email FROM user WHERE email = '$_POST[email]'");
+    if (mysqli_num_rows($check) > 0) {
+        print_r($check);
+        $msg = "User already Exist";
+    } else {
+        $result = mysqli_query($conn, "INSERT INTO user(first_name,last_name,email,password,role) VALUES('$_POST[fname]','$_POST[lname]','$_POST[email]','$_POST[password]','$_POST[role]')");
+        if (!$result) {
+            echo mysqli_error($conn);
+        }
+    }
+}
+?>
   <div id="admin-content">
       <div class="container">
           <div class="row">
@@ -7,6 +32,16 @@
               </div>
               <div class="col-md-offset-3 col-md-6">
                   <!-- Form Start -->
+                  <?php 
+                if(isset($msg))
+                {
+                  echo "<div class='alert alert-warning alert-dismissible' role='alert'>
+                        <button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button>
+                        <strong>{$msg}</strong>
+                    </div>";
+
+                }
+                ?>
                   <form  action="" method ="POST" autocomplete="off">
                       <div class="form-group">
                           <label>First Name</label>
@@ -17,8 +52,8 @@
                           <input type="text" name="lname" class="form-control" placeholder="Last Name" required>
                       </div>
                       <div class="form-group">
-                          <label>User Name</label>
-                          <input type="text" name="user" class="form-control" placeholder="Username" required>
+                          <label>Email Address</label>
+                          <input type="email" name="email" class="form-control" placeholder="Email Address" required>
                       </div>
 
                       <div class="form-group">
@@ -28,8 +63,13 @@
                       <div class="form-group">
                           <label>User Role</label>
                           <select class="form-control" name="role" >
-                              <option value="0">Normal User</option>
-                              <option value="1">Admin</option>
+                              <option >Select Role</option>
+                              <?php 
+                              while($row = mysqli_fetch_assoc($roles))
+                              {
+                                  echo"<option value='$row[id]'> $row[role]</option>";
+                              }
+                              ?>
                           </select>
                       </div>
                       <input type="submit"  name="save" class="btn btn-primary" value="Save" required />
